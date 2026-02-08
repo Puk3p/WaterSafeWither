1e82670606db82f2108c6112aa8898f654547a94
Puk3p <gheorghe.lupu2@student.tuiasi.ro>
2026-02-08 22:38:20 +0200

    Initial commit

---
+++ src/main/kotlin/ro/puk3p/waterSafeWither/WaterSafeWither.kt
@@ -0,0 +1,14 @@
+package ro.puk3p.waterSafeWither
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class WaterSafeWither : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
